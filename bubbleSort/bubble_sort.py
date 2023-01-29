cc1db9272cab5d2cc90e5a83fc8849469fdbf329
prime-infinity <soe.a.prime@gmail.com>
2023-01-29 08:52:45 -0800

    bubble sort python

--- bubbleSort/bubble_sort.py
+++ bubbleSort/bubble_sort.py
@@ -1,4 +1,13 @@
 list = [5, 2, 7, 4, 1, 6, 3, 0]
 
-for i in range(len(list)):
-    print(list[i])
+
+def swap_values(fwd, flw):
+    [list[fwd], list[flw]] = [list[flw], list[fwd]]
+
+
+for i in range(1, len(list)):
+    for j in range(0, len(list)-1):
+        if list[i] < list[j]:
+            swap_values(i, j)
+
+print(list)
