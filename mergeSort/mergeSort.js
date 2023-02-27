f7ca62678fe1a26fe4fce401d529b372b4313014
prime-infinity <soe.a.prime@gmail.com>
2023-02-27 01:26:47 -0800

    exploring merge sort

--- mergeSort/mergeSort.js
+++ mergeSort/mergeSort.js
@@ -5,3 +5,22 @@ leftList = list.slice(0, midPoint);
 rightList = list.slice(midPoint, list.length);
 console.log(leftList);
 console.log(rightList);
+
+//now lets try to merge the two halves into a new array
+let newMerged = [];
+for (let i = 0; i < leftList.length; i++) {
+  //const e = leftList[i];
+  //console.log(e);
+  //console.log(i);
+  //console.log(leftList[i]);
+  //console.log(rightList[i]);
+  if (leftList[i] < rightList[i]) {
+    //console.log(`${leftList[i]} is less than ${rightList[i]}`);
+    newMerged.push(leftList[i]);
+  } else {
+    //console.log(`${rightList[i]} is less than ${leftList[i]}`);
+    newMerged.push(rightList[i]);
+  }
+}
+
+console.log(newMerged);
