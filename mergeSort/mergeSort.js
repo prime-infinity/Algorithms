413952220a671e9ceb1f14c5f68d744818c6c3b6
prime-infinity <soe.a.prime@gmail.com>
2023-02-27 10:46:06 -0800

    exploring merge sort

--- mergeSort/mergeSort.js
+++ mergeSort/mergeSort.js
@@ -3,24 +3,36 @@ let midPoint = Math.floor(list.length / 2);
 //console.log(midPoint);
 leftList = list.slice(0, midPoint);
 rightList = list.slice(midPoint, list.length);
-console.log(leftList);
-console.log(rightList);
+//console.log(leftList);
+//console.log(rightList);
 
 //now lets try to merge the two halves into a new array
 let newMerged = [];
-for (let i = 0; i < leftList.length; i++) {
-  //const e = leftList[i];
-  //console.log(e);
-  //console.log(i);
-  //console.log(leftList[i]);
-  //console.log(rightList[i]);
+
+//the below is not the correct thing to do
+//it simply compares the lists and appends the
+//minimum of the comparism to the new list
+/*for (let i = 0; i < leftList.length; i++) {
   if (leftList[i] < rightList[i]) {
-    //console.log(`${leftList[i]} is less than ${rightList[i]}`);
     newMerged.push(leftList[i]);
   } else {
-    //console.log(`${rightList[i]} is less than ${leftList[i]}`);
     newMerged.push(rightList[i]);
   }
-}
+}*/
 
-console.log(newMerged);
+z = 0;
+let leftPointer = 0;
+let rightPointer = 0;
+while (z < 12) {
+  if (leftList[leftPointer] < rightList[rightPointer]) {
+    newMerged.push(leftList[leftPointer]);
+    leftPointer += 1;
+  } else {
+    newMerged.push(rightList[rightPointer]);
+    rightPointer += 1;
+  }
+  z++;
+}
+console.log(leftPointer);
+console.log(leftList.length, rightList.length);
+//console.log(newMerged);
